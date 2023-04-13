ec0db12359ca3a887ceae18b04970c84ea63050f
Chellakiranmai <kiranmai.chella@gmail.com>
2023-04-13 20:53:45 +0530

    starbucks clone

---
+++ Star Bucks clone/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything){
+    document.querySelector('.frap1').src = anything;
+}
+function changeCirclecolour (color){
+    const circle =document.querySelector('.circle');
+    circle.style.background = color;
+
+}
